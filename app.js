683be8cd3d3b103a57d1e09cf2d5b3d210449730
justanooooob <okarel175@gmail.com>
2023-06-01 05:14:41 +0000

    test

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const conn = require("./config/db");
 
 app.use(express.json())
 
-app.get('/get-animals', function (req, res) {
+/*app.get('/get-animals', function (req, res) {
     const queryStr = 'SELECT id, name, description FROM animals WHERE deleted_at IS NULL';
     conn.query(queryStr, (err, results) => {
       if (err) {
@@ -17,13 +17,61 @@ app.get('/get-animals', function (req, res) {
         });
       }
     })
-  })
+  })*/
+  
+const resultsPerPage = 5;
+
+app.get('/get-animals', (req, res) => {
+    let sql = 'SELECT id, name, description FROM animals WHERE deleted_at IS NULL';
+    conn.query(sql, (err, result) => {
+        if(err) throw err;
+        const numOfResults = result.length;
+        const numberOfPages = Math.ceil(numOfResults / resultsPerPage);
+        let page = req.query.page ? Number(req.query.page) : 1;
+        if(page > numberOfPages){
+            res.redirect('/get-animals?page='+encodeURIComponent(numberOfPages));
+        }else if(page < 1){
+            res.redirect('/get-animals?page='+encodeURIComponent('1'));
+        }
+        //Determine the SQL LIMIT starting number
+        const startingLimit = (page - 1) * resultsPerPage;
+        //Get the relevant number of POSTS for this starting page
+        sql = `SELECT id, name, description FROM animals  WHERE deleted_at IS NULL LIMIT ${startingLimit},${resultsPerPage}`;
+        conn.query(sql, (err, result)=>{
+            if(err) throw err;
+            let iterator = (page - 5) < 1 ? 1 : page - 5;
+            let endingLink = (iterator + 9) <= numberOfPages ? (iterator + 9) : page + (numberOfPages - page);
+            if(endingLink < (page + 4)){
+                iterator -= (page + 4) - numberOfPages;
+            } 
+            res.status(200).json({data: result, page, iterator, endingLink, numberOfPages});
+        });
+    });
+});
 
   app.get('/get-animal-by-id', function (req, res) {
     const param = req.query;
     const id = param.id;
     const queryStr = 'SELECT id, name, description FROM animals WHERE id = ? AND deleted_at IS NULL';
     const values = [id];
+    conn.query(queryStr, values, (err, results) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.status(200).json({
+          "success" : true,
+          "message" : "Sukses menampilkan data",
+          "data" : results
+        });
+      }
+    })
+  })
+
+  app.get('/get-animal-by-name', function (req, res) {
+    const param = req.query;
+    const name = param.name;
+    const queryStr = 'SELECT id, name, description FROM animals WHERE name LIKE ? AND deleted_at IS NULL';
+    const values = ['%' + name + '%'];
     conn.query(queryStr, values, (err, results) => {
       if (err) {
         console.log(err);
